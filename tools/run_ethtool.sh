cfee3f11ecedaf0ca46ed104375ea3dd58a3fee5
Sean Choi <yo2seol@gmail.com>
2018-12-17 14:53:29 -0800

    Turn off checksumming

--- tools/run_ethtool.sh
+++ tools/run_ethtool.sh
@@ -8,5 +8,7 @@ ifup $1
 /sbin/ethtool --coalesce $1 tx-frames 1
 
 # Adding route
-ip route add 30.30.30.105/32 dev vf0_1
-ip route add 20.20.0.0/16 dev vf0_1
+ip route add 30.30.30.105/32 dev $1
+ip route add 20.20.0.0/16 dev $1
+
+ethtool -K $1 tx off rx off
