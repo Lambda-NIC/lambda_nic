711ec86130aec8c35f7d8e3ec4374733d661a676
Sean Choi <yo2seol@gmail.com>
2018-12-21 16:05:04 -0800

    Adding some tools

--- tools/add_route.sh
+++ tools/add_route.sh
@@ -1,3 +1,5 @@
 #ip route add 30.30.30.105/32 dev enp94s0np1
+ip route del 20.20.20.0/24
+ip route del 20.20.0.0/16
 ip route add 30.30.30.105/32 dev vf0_1
 ip route add 20.20.0.0/16 dev vf0_1
--- tools/run_ethtool.sh
+++ tools/run_ethtool.sh
@@ -8,6 +8,8 @@ ifup $1
 /sbin/ethtool --coalesce $1 tx-frames 1
 
 # Adding route
+ip route del 20.20.0.0/16
+ip route del 20.20.20.0/24
 ip route add 30.30.30.105/32 dev $1
 ip route add 20.20.0.0/16 dev $1
 
