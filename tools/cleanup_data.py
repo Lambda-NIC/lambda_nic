29e10cc039a050a5811a33d678dd777f5535534d
Sean Choi <yo2seol@gmail.com>
2019-01-02 15:33:14 -0800

    Fixing bug with data cleaner

--- tools/cleanup_data.py
+++ tools/cleanup_data.py
@@ -4,12 +4,16 @@ filepath = sys.argv[1]
 function = sys.argv[2]
 
 function_name = "/function/%s" % function
+system_call = "/system/function/%s" % function
 
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
        if function_name not in line:
+           line = fp.readline()
+           continue
+       elif system_call in line:
            line = fp.readline()
            continue
        else:
