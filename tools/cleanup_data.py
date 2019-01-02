6cc7436d3b84ac3c53a97d0fddc5f4911056ab0b
Sean Choi <yo2seol@gmail.com>
2019-01-02 14:36:32 -0800

    Adding script to cleanup data

---
+++ tools/cleanup_data.py
@@ -0,0 +1,18 @@
+import sys
+
+filepath = sys.argv[1]
+function = sys.argv[2]
+
+function_name = "/function/%s" % function
+
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       if function_name not in line:
+           line = fp.readline()
+           continue
+       else:
+           print(line.strip().split()[9])
+       line = fp.readline()
+       cnt += 1
