fae013167095be7627a62da8e28e78e773cd0c95
Sean Choi <yo2seol@gmail.com>
2019-01-02 10:42:24 -0800

    Adding new docker files for memcached and python script to make requests

---
+++ tools/make_job_request.py
@@ -0,0 +1,59 @@
+import sys
+import requests
+import timeit
+import threading
+
+def make_request(url, params, num_requests):
+    for i in range(num_requests):
+        res = requests.post(url, data = params)
+        print res.status_code, res.text
+
+BASE_URL =  "http://172.24.90.32:8001/api/v1/namespaces/openfaas/services/http:gateway:/proxy/function/"
+
+job_type = sys.argv[1]
+job_id = int(sys.argv[2])
+num_requests = int(sys.argv[3])
+num_threads = int(sys.argv[4])
+if num_threads < 1 or num_threads > 8:
+    print "Num threads should be >= 1 and <= 8 "
+    sys.exit(1)
+
+url = None
+params = None
+
+if job_type == "lambdanic":
+    url = BASE_URL + "lambdanictest"
+    params = str(job_id)
+elif job_type == "docker":
+    if job_id == 0:
+        url = BASE_URL + "simpleserver"
+    elif job_id == 1:
+        url = BASE_URL + "imagetransform"
+    elif job_id == 2 or job_id == 3 or job_id == 4:
+        url = BASE_URL + "simplememcached"
+        params = str(job_id)
+else:
+    print "Invalid job type"
+    sys.exit(1)
+
+
+# Create a list of jobs and then iterate through
+# the number of threads appending each thread to
+# the job list
+jobs = []
+for i in range(0, num_threads):
+    out_list = list()
+    thread = threading.Thread(target=make_request(url, params, num_requests))
+    jobs.append(thread)
+
+
+tic = timeit.default_timer()
+# Start the threads (i.e. calculate the random number lists)
+for j in jobs:
+    j.start()
+
+# Ensure all of the threads have finished
+for j in jobs:
+    j.join()
+toc = timeit.default_timer()
+print "Total Time: ", toc - tic
