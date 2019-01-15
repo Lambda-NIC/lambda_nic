e5aab7ec3a9377ca3386587728fe53b619699d71
Sean Choi <yo2seol@gmail.com>
2019-01-14 21:03:35 -0800

    Adding sender

--- tools/send_lambdanic_test_pkt_from_master_max_rate.py
+++ tools/send_lambdanic_test_pkt_from_master_max_rate.py
@@ -3,20 +3,19 @@ send_lambdanic_test_pkt.py
 Sends a udp packet to the IP and eth of choice.
 """
 
-from scapy.all import *
-from time import sleep
-from scapy.all import Ether, IP, UDP, Raw
-from threading import Thread, Event
 import struct
-from datetime import datetime
+import timeit
+import sys
+import socket
 from multiprocessing import Process, Pool
 
 
 DST_NUM = int(sys.argv[1])
 SRC_UDP_PORT = 2222
-DST_UDP_PORT = int(sys.argv[])
+DST_UDP_PORT = int(sys.argv[2])
 NUM_PROCESSES = int(sys.argv[3])
 NUM_PACKETS = int(sys.argv[4])
+BUFFER_SIZE = 1024
 
 IFACE = "eno1np0"
 SRC_ETH_ADDR = "b0:26:28:1a:75:60"
@@ -39,13 +38,14 @@ def query_workload(job_id):
     UDPClientSocket.sendto(data, serverAddressPort)
 
     #Receive message from the server
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
-    print(msg)
+    msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
+    return msgFromServer[0]
 
 if __name__ == "__main__":
     # TODO: Create a list of 5,6,7s
     workloads = [5,6,7] * int(NUM_PACKETS/3)
-    with Pool(processes=NUM_PROCESSES) as pool:
-        print(pool.map(query_workload, workloads))
-
+    tic = timeit.default_timer()
+    with Pool(NUM_PROCESSES) as pool:
+        pool.map(query_workload, workloads)
+    toc = timeit.default_timer()
+    print ("Time taken: %f" % (toc - tic))
